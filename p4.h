037ca2466728b7ef3b0b7857bee1b5dd7d5c85b4
kbansal <kbansal@ncsu.edu>
2015-11-20 11:21:25 -0500

    Create p4.h

---
+++ p4.h
@@ -0,0 +1,19 @@
+/*
+ * p4.h
+ *
+ *  Created on: 20-Nov-2015
+ *      Author: Kushal
+ */
+
+#ifndef P4_H_
+#define P4_H_
+
+
+typedef struct ipRec{
+	int index;
+	char ip[16];
+	int port;
+}ipRec;
+
+
+#endif /* P4_H_ */
